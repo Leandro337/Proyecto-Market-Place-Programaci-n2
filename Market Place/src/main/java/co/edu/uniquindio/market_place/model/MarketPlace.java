98aa229ba6ce8feba82f12f7f27d4301b4432e2f
_Jenn_ <jenniferandrea435@gmail.com>
2024-10-25 08:46:29 -0500

    Cambios en la clase vendedor, implementacion del crud vendedor (Mateo, Jennifer)

--- Market Place/src/main/java/co/edu/uniquindio/market_place/model/MarketPlace.java
+++ Market Place/src/main/java/co/edu/uniquindio/market_place/model/MarketPlace.java
@@ -5,34 +5,95 @@ import co.edu.uniquindio.market_place.service.IVendedorCRUD;
 import java.util.ArrayList;
 
 public class MarketPlace implements IVendedorCRUD {
+    private List<Vendedor> listaVendedores;
+
+    public MarketPlace(){
+        this.listaVendedores = new ArrayList<>();
+    }
 
     @Override
-    public boolean crearVendedor(Vendedor vendedor) {
+    public boolean crearVendedor(String cedula, String nombre, String apellido, String direccion, String usuario, String contraseña) {
+        Vendedor vendedorEncontrado = verificarVendedor(cedula);
+
+        if (vendedorEncontrado == null) {
+            Vendedor vendedor = Vendededor.builder()
+                    .nombre(nombre)
+                    .cedula(cedula)
+                    .apellido(apellido)
+                    .usuario(usuario)
+                    .contraseña(contraseña)
+                    .direccion(direccion)
+                    .build();
+
+            getListVendedores().add(vendedor);
+
+            return true;
+        }
+        return false;
+    }
+
+
+    @Override
+    public boolean eliminarVendedor(String cedula) {
+        Vendedor vendedorEncontrado = verificarVendedor(cedula);
+        if (vendedorEncontrado != null) {
+            getListaVendedores().remove(vendedorEncontrado);
+            return true;
+        }
         return false;
     }
 
     @Override
-    public boolean eliminarVendedor(int id) {
+    public boolean actualizarVendedor (String nombre, String apellido, String direccion, String usuario, String contraseña){
+        Vendedor vendedorEncontrado = verificarVendedor(cedula);
+        if (vendedorEncontrado != null) {
+            vendedorEncontrado.setNombre(nombre);
+            vendedorEncontrado.setApellido(apellido);
+            vendedorEncontrado.setDireccion(direccion);
+            vendedorEncontrado.setUsuario(usuario);
+            vendedorEncontrado.setContraseña(contraseña);
+            return true;
+        }
         return false;
     }
 
-    @Override
-    public boolean actualizarVendedor(String cedula, String nombre, String apellido, int edad) {
-        return false;
-    }
 
     @Override
     public Vendedor obtenerVendedor(int cedula) {
-        return null;
+        for(Vendedor vendedor : listaVendedores) {
+            if (vendedor.getCedula().equels(cedula)) {
+                return vendedor.getNombre();
+            }
+        }
     }
 
-    @Override
-    public ArrayList<Vendedor> listaVendedores() {
-        return null;
-    }
 
     @Override
-    public boolean verificarVendedor(String cedula) {
+    public boolean verificarVendedorExistente (String cedula){
+        if (verificarVendedor(cedula) == null){
+            return true;
+        }
         return false;
+    }
+
+    public Vendedor verificarVendedor(String cedula) {
+        Vendedor vendedorExistente = null;
+
+        for (Vendedor vendedor : listaVendedores) {
+            if (vendedor.getCedula().equals(cedula)) {
+                vendedorExistente = vendedor;
+                break;
+            }
+        }
+        return vendedorExistente;
+    }
+
+    @Override
+    public List<Vendedor> listaVendedores() {
+        return listaVendedores;
+    }
+
+    public void setListaVendedores(List<Vendedor> listaVendedores) {
+        this.listaVendedores = listaVendedores;
     }
 }
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 En este proyecto se desarrolla la codificación de un Market Place como proyecto final de la asignatura Programación 2
 Presentado por:
-    Oscar Leandro Agudelo Franco
-    Jennifer Andrea Cortés Chavarro
+    Oscar Leandro Agudelo Franco,
+    Jennifer Andrea Cortés Chavarro,
     Mateo Garrido Rios
