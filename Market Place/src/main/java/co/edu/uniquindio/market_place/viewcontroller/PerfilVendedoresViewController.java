44b2f928ed58a29221c087e59e787b861031599e
Mateo <mateogarridorios@gmail.com>
2024-10-26 22:40:16 -0500

    Creacion de los controller de los archivos fxml

---
+++ Market Place/src/main/java/co/edu/uniquindio/market_place/viewcontroller/PerfilVendedoresViewController.java
@@ -0,0 +1,20 @@
+package co.edu.uniquindio.market_place.viewcontroller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
+public class PerfilVendedoresViewController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
