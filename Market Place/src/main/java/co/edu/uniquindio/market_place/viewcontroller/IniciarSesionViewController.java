4fa5d8b21a5b8561260333fbea6244eb9dd88b85
Mateo <mateogarridorios@gmail.com>
2024-11-12 22:24:38 -0500

    Adicion iniciarSesion, view y controller, cambios PerfilVendedores, adicion ProductoVendedor, view y controller

--- Market Place/src/main/java/co/edu/uniquindio/market_place/viewcontroller/IniciarSesionViewController.java
+++ Market Place/src/main/java/co/edu/uniquindio/market_place/viewcontroller/IniciarSesionViewController.java
@@ -1,5 +1,35 @@
 package co.edu.uniquindio.market_place.viewcontroller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class IniciarSesionViewController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private PasswordField iniciarContrasenaTxt;
+
+    @FXML
+    private Button iniciarSesionButton;
+
+    @FXML
+    private TextField iniciarUsuarioTxt;
+
+    @FXML
+    private Button registrarButton;
+
+    @FXML
+    void initialize() {
+
+    }
 
 }
---
+++ Market Place/src/main/java/co/edu/uniquindio/market_place/viewcontroller/ProductoVendedorViewController.java
@@ -0,0 +1,24 @@
+package co.edu.uniquindio.market_place.viewcontroller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class ProductoVendedorViewController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button likeButton;
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
